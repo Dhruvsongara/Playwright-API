6a01605c649b09586cd91ba82deb6f348076d066
Dhruvsongara <dhruvsongara16@gmail.com>
2026-07-08 11:22:28 +0530

    network interception

---
+++ tests/apis/network-interception.spec.ts
@@ -0,0 +1,39 @@
+import { test, expect } from "@playwright/test";
+
+test("Network interception mock API response", async ({ page }) => {
+  await page.route("**/v2/pet/findByStatus?status=available", async (route) => {
+    await route.fulfill({
+      status: 200,
+      contentType: "application/json",
+      body: JSON.stringify([
+        {
+          id: 101,
+          name: "Mock Pet",
+          status: "available",
+        },
+      ]),
+    });
+  });
+
+  const response = await page.request.get(
+    "https://petstore.swagger.io/v2/pet/findByStatus?status=available"
+  );
+
+  expect(response.status()).toBe(200);
+
+  const body = await response.json();
+
+  console.log(body);
+
+//   expect(body[0].name).toBe("doggie");
+});
+
+test("network interception for unicorstore", async ({page})=>{
+
+    await page.route("https://shop.unicornstore.in/", async (route)=>{
+
+        await route.fulfill({
+            status: 200,
+        });
+    });
+});
